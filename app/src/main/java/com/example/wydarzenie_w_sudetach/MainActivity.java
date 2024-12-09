09cb45a06a1a9d9a413ce007dbc5e6faecfae8a4
LordOwing <michallolo12345@gmail.com>
2024-12-09 08:49:22 +0100

    09.12

--- app/src/main/java/com/example/wydarzenie_w_sudetach/MainActivity.java
+++ app/src/main/java/com/example/wydarzenie_w_sudetach/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.wydarzenie_w_sudetach;
 
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,6 +34,38 @@ public class MainActivity extends AppCompatActivity {
                 likeCount--;
                 updateLikeCount();
             }
+        });
+        TextView text_2 = findViewById(R.id.obszar);
+        findViewById(R.id.zapisz).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = ((EditText) findViewById(R.id.form1)).getText().toString().trim();
+
+
+                String pass = ((EditText) findViewById(R.id.form2)).getText().toString().trim();
+                String passConf = ((EditText) findViewById(R.id.form3)).getText().toString().trim();
+
+
+                if (email.indexOf('@')!=-1) {
+                    text_2.setText("Obszar komunikatów: \n E-mail nie zawiera @");
+                }
+                else if (!pass.equals(passConf)) {
+                    text_2.setText("Obszar komunikatów: \n Hasła się różnią");
+                } else {
+                    text_2.setText("Obszar komunikatów: \n Witaj "+ email);
+
+                }
+            }
+        });
+        findViewById(R.id.zobacz).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = ((EditText) findViewById(R.id.form1)).getText().toString().trim();
+                text_2.setText("Obszar komunikatów: " + email);
+            }
+
+
         });
     }
 
